46abec3cc9289a74c504888211ded47929643250
matthew pyle <Matthew.Pyle@noaa.gov>
2023-01-09 18:56:50 +0000

    Adds a stub read for a problematic part of the ARW code, and updates the overall build_all.sh script based on the SPA experience.

--- sorc/build_all.sh
+++ sorc/build_all.sh
@@ -23,6 +23,7 @@ fi
 # just in case - needed for FV3 build
 # module load python/2.7.13
 
+module load intel/19.1.3.304
 module load python/3.8.6
 
 # /gpfs/dell1/nco/ops/nwtest/upgrade_utils.v0.0.2/exec/checkoutsidecompilefiles ./build_all.sh >& build_all_fv3.log
