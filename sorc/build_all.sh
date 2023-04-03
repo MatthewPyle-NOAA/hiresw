0e0430b31c137289b540252c3bedfc4719733f4f
matthew pyle <Matthew.Pyle@noaa.gov>
2023-04-03 12:10:36 +0000

    Pushes change to how UFS Utils is checked out for the build.  Manage externals suddenly started having issues within HiresW, so change to a direct clone of the proper branch.

--- sorc/build_all.sh
+++ sorc/build_all.sh
@@ -7,7 +7,8 @@
 # 1st time only
 if [ ! -e fv3/hireswfv3_utils.fd ]
 then
-./manage_externals/checkout_externals
+# ./manage_externals/checkout_externals
+git clone -b hrefv3.1-wcoss2 https://github.com/GeorgeGayno-NOAA/UFS_UTILS.git fv3/hireswfv3_utils.fd
 else
 echo "already ran checkout_externals"
 fi
@@ -20,14 +21,15 @@ then
 ./link_fix.sh
 fi
 
-# just in case - needed for FV3 build
-# module load python/2.7.13
+# just in case - needed for FV3 model build
 
 module load intel/19.1.3.304
 module load python/3.8.6
 
 # /gpfs/dell1/nco/ops/nwtest/upgrade_utils.v0.0.2/exec/checkoutsidecompilefiles ./build_all.sh >& build_all_fv3.log
+
 ./build_all.sh >& build_all_fv3.log
+
 
 #################################
 # ARW build
